70c29bff27bca8dd3d2887c5733407acba623722
Unknown <karsin2605@gmail.com>
2018-11-12 23:41:30 +0100

    trial with watershed

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function whenDocumentLoaded(action) {
 }
 
 whenDocumentLoaded(() => {
-	var urlWaterShed = "watershed.geojson";
+	var urlWaterShed = "/watershed.geojson";
 	var urlAzote = "n_export.json";
 	var urlPhosphore = "p_export.json";
 
