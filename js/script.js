cf487e3506877296598780d165c37dde2a8497d3
Alex Ferragni <pokemaniak@sunrise.ch>
2018-11-13 11:11:32 +0100

    correct url

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function whenDocumentLoaded(action) {
 }
 
 whenDocumentLoaded(() => {
-	var urlWaterShed = "{{ site.baseurl }}/watershed.geojson";
+	var urlWaterShed = "raw/master/watershed.geojson";
 	var urlAzote = "n_export.json";
 	var urlPhosphore = "p_export.json";
 
