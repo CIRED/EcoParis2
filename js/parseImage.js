43f4607ef3d5cabcba6965f9a8ec19645d33b415
Alex Ferragni <pokemaniak@sunrise.ch>
2018-12-05 14:41:45 +0100

    git status

--- js/parseImage.js
+++ js/parseImage.js
@@ -1,3 +1,24 @@
+
+// ========== INSTRUCTIONS ==========
+/*
+ *	If you have to change either the "intercommunalites.geojson" file or the voronois, then you'll need to recompute containent of pixels with this page
+ *
+ *	SETPS:
+ *		- down there, change the name of the geojson you want to change (one of them or both)
+ *		- if you don't want to recompute voronois containment, comment between [1] and [2] (around lines 200)
+ *		- if you don't want to recompute intercomm containment, comment between [2] and [3] (around lines 200)
+ *		- run this page locally, "python -m http.server"
+ *		- load the page, and wait. It might take a while, certainly around 30-40 minutes. (You might want to leave your browser as it is and not touch anything, because
+ *			while computing on my computer, reducing the browser or even simply switching tab to another caused the page to crash and reload.)
+ *		- even better if you want to make sure it did not crash: don't uncomment between [1] and [3], load the page and open the javascript console (so before it is working,
+ *			because while computing I wasn't able to open the console), then uncomment save and reload, the counter will indicate progression.
+ *		- Finally, when the page has loaded you can download you files at the bottom of the page
+ */
+
+var InterCommFilePath = "intercommunalites.geojson"
+var VoronoiFilePath = "public/data/backup/voronois-10.json"
+
+
 function whenDocumentLoaded(action) {
 	if (document.readyState === "loading") {
 		document.addEventListener("DOMContentLoaded", action);
@@ -8,8 +29,8 @@ function whenDocumentLoaded(action) {
 }
 
 whenDocumentLoaded(() => {
-	var urlDPT = "intercommunalites.geojson";
-	var urlVoronoi = "public/data/backup/voronois-10.json";
+	var urlDPT = InterCommFilePath;
+	var urlVoronoi = VoronoiFilePath;
 
 	// Load the JSON file(s)
 	queue()
@@ -26,7 +47,7 @@ whenDocumentLoaded(() => {
 	        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	    });
 
-	    var tl = new L.LatLng(49.2485668,1.4403262)
+	    var tl = new L.LatLng(49.2485668,1.4403262) //focused on Paris
 	    var br = new L.LatLng(48.1108602,3.5496114)
 
 	    // Zoomed on Paris
@@ -44,13 +65,6 @@ whenDocumentLoaded(() => {
 
 	    var svg = d3.select("#ParisMap").select("svg")
 
-	    var imgs = svg.selectAll("image").data([0,0]);
-	    imgs.enter()
-	        .append("svg:image")
-	        .attr('x', 0)
-	        .attr('y', 0)
-	        .attr("xlink:href", "")
-
 	    function projectPoint(x, y) {
 	        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
 	        this.stream.point(point.x, point.y);
@@ -61,31 +75,13 @@ whenDocumentLoaded(() => {
 	        .projection(transform);
 
 	    var watersheds = svg.append("g").selectAll("path")
-	        .data([voronoi_shape.features[123],voronoi_shape.features[124],voronoi_shape.features[125]])
+	        .data(voronoi_shape.features)
 	        .enter().append('path')
 	            .attr('d', path)
 	            .attr('vector-effect', 'non-scaling-stroke')
 	            .style('stroke', "#000")
 	            .attr("fill","none")
 
-	    var img = document.getElementById('my-image');
-	    var image_width = img.width
-	    var image_height = img.height
-
-	    var canvas = document.createElement("canvas")
-	        canvas.width=image_width//image_data.width
-	        canvas.height=image_height//image_data.width
-
-	    var paths = ['data/points_0_out.txt','data/points_1_out.txt',
-            'data/points_2_out.txt','data/points_3_out.txt',
-            'data/points_4_out.txt','data/points_5_out.txt',
-            'data/points_6_out.txt','data/points_7_out.txt']
-
-	    var polygons = []
-	    for (var i=0; i<paths.length;++i){
-	    	polygons[i] = svg.append("g").attr("class","polygons")
-	    }
-        
         map.on("viewreset", update);
 	    update();
         
@@ -93,110 +89,8 @@ whenDocumentLoaded(() => {
 	        var width = (map.latLngToLayerPoint(br).x-map.latLngToLayerPoint(tl).x)
 	        var height = (map.latLngToLayerPoint(br).y-map.latLngToLayerPoint(tl).y)
             
-	        imgs.attr("transform", 
-	            function(d) { 
-	                var point = map.latLngToLayerPoint(tl)
-	                return "translate("+ 
-	                    point.x +","+ 
-	                    point.y +")";
-	            }
-	        )
-            
-	        imgs.attr("width", 
-	            function(d) { 
-	                return width;
-	            }
-	        )
-            
-	        imgs.attr("height", 
-	            function(d) { 
-	                return height;
-	            }
-	        )
-	        imgs.attr("xlink:href",value)
-            
 	        watersheds.attr("d",path)
 
-            
-            
-            
-            /*var fc = {'type': 'FeatureCollection','features': []}
-            var poly = '{"type": "Feature","properties": {},"geometry": {"type": "Polygon","coordinates": []}}'
-                
-            for (i=0;i<paths.length;i++){
-                dpt_index = i;
-                data = loadFile(paths[i])
-                data_splitted = data.split(",")
-                data_real=[]
-                for(j=0;j<data_splitted.length;j++){
-                    if(j==0){
-                        
-                    }
-                    else if(j%2==1){
-                        data_real.push(parseFloat(""+data_splitted[j]))
-                    }
-                    else{
-                        data_real.push(parseFloat(""+data_splitted[j].slice(0,7)))
-                    }
-                }
-
-                var json_data = []
-                for(k=0;k<data_real.length;k=k+2){
-                    json_data.push({x:data_real[k], y:data_real[k+1]})
-                }
-
-                function imageToLatLng(x,y){
-                    var tx = x/(image_width-1)
-                    var ty = y/(image_height-1)
-                    return L.latLng(tl.lat * (1-ty) + br.lat * ty, tl.lng * (1-tx) + br.lng * tx)
-                }
-
-                var voronoi = d3.voronoi()
-                    .x(function(d) { return map.latLngToLayerPoint(imageToLatLng(d.x,d.y)).x; })
-                    .y(function(d) { return map.latLngToLayerPoint(imageToLatLng(d.x,d.y)).y; })
-                    .extent([[-100000, -100000], [100000,100000]]);//[canvas.width, canvas.height]]);
-                    
-                voronoi_clipped_data = voronoi(json_data).polygons().map(function(d) {
-                        var mapped = dpt_shape.features[dpt_index].geometry.coordinates[0].map(function(p) {
-                            projected_pt = map.latLngToLayerPoint(L.latLng(p[1],p[0]))
-                            return [projected_pt.x,projected_pt.y]
-                        });
-                        var polygon_mask = d3.geom.polygon(mapped)
-                        var polygon = d3.geom.polygon(d)
-                        var clipped = polygon.clip(polygon_mask) 
-                        return clipped
-                    })
-                    
-                var voronoi_clipped = polygons[i]
-                    .selectAll("path")
-                    .data(voronoi_clipped_data)
-                    .enter()
-                    .append("path")
-                    .attr("d",function(d){return ((d != null && d.length != 0) ? "M"+d.join("L")+"Z" : "") })
-                    .style("stroke", function(d){  return "#000000"} )
-                    .style("fill","none");
-
-                polygons[i]
-                    .selectAll("path")
-                    .attr("d",function(d){return ((d != null && d.length != 0) ? "M"+d.join("L")+"Z" : "") })
-                    
-                voronoi_clipped_data.forEach(p => {
-                    let feature = JSON.parse(poly)
-                    p.reverse().push(p[0])
-                    feature.geometry.coordinates.push(p)
-                    fc.features.push(feature)
-                })
-            }
-            //todo: map fc to latlng
-            fc.features.forEach((feature,i) =>{
-            	feature.geometry.coordinates.forEach((coordinates,j) => {
-            		coordinates.forEach((point,k) => {
-            			var latLng = map.layerPointToLatLng(L.point(point[0],point[1]))
-            			fc.features[i].geometry.coordinates[j][k] = [latLng.lng,latLng.lat]
-            		})
-            	})
-            })
-        	download(JSON.stringify(fc, null, 2), 'sd-voronoi.json', "json", 8)*/
               
 	    }
 	    
@@ -218,145 +112,78 @@ whenDocumentLoaded(() => {
 			return result;
 		}
 
-	    canvas.getContext('2d').drawImage(img, 0, 0, image_width, image_height);
-	    var context = canvas.getContext('2d');
+		var width = 1977
+		var height = 1590
 
-	    var pixels = context.getImageData(0, 0, canvas.width, canvas.height).data
+		var interComm_cont_data=[]
+		var voronoi_cont_data=[]
 
-	    var imageData=context.createImageData(image_width, image_height);
-	    // The property data will contain an array of int8
-	    var data=imageData.data;
-
-	    
-		var pixels_data=[]
-		var depts_cont_data=[]
-		var string=[]
-
-		var voronoi_means = []
-		var voronoi_count = []
-		for (var i=0; i<8; ++i){
-			string[i]=""
-		}
-		for (var i=0; i<voronoi_shape.features.length;++i){
-			voronoi_count[i]=0
-			voronoi_means[i]=0
-		}
-		console.log(dpt_shape.features)
-	    for (var i=0; i<canvas.height*canvas.width; i++) {
-	        var px = i%canvas.width
-	        var py = Math.floor(i/canvas.width)
+	    for (var i=0; i<height*width; i++) {
+	        var px = i%width
+	        var py = Math.floor(i/width)
 	        //var value = getRasterPixelValue(i%canvas.width,i/canvas.width)
-	        if(px >= 0 && px < image_width && py >= 0 && py < image_height){
-	            pos = i*4
+	        if(px >= 0 && px < width && py >= 0 && py < height){
 
-	            var value = pixels[i*4]
-	            //var v = (value - mean)/(std*2) + 0.5;
-	            data[pos]   = parseInt(getColour(value),16) & 255
-	            data[pos+1]   = (parseInt(getColour(value),16) >> 8) & 255
-	            data[pos+2]   = (parseInt(getColour(value),16) >> 16) & 255
-	            if (pixels[i*4]==0){
-	                data[pos+3]=0; // alpha (transparency)
-	            }
-	            else{
-	                data[pos+3]=180;
-	            }
-	        	pixels_data[i]=value;
-	        	depts_cont_data[i]=0;
+	        	interComm_cont_data[i]=0;
+	        	voronoi_cont_data[i]=0;
 
-	        	var tx = px/(canvas.width-1)
-	        	var ty = py/(canvas.height-1)
+	        	var tx = px/(width-1)
+	        	var ty = py/(height-1)
 
 	        	var lat = tl.lat * (1-ty) + br.lat*ty
 	        	var lng = tl.lng * (1-tx) + br.lng*tx
+
+	        	//
+	        	// ============ [1], interComm below
+	        	//
+
 	        	/*for (var k=0; k<dpt_shape.features.length; ++k){
 	        		if (d3.geoContains(dpt_shape.features[k],[lng,lat])){
-	        			//console.log(px,py,tl.lng * (1-tx) + br.lng*tx,tl.lat * (1-ty) + br.lat*ty)
-		        		//for (var j=0; j<value/51; ++j){
-			        	//	string[k] += ""+px+","+py+"\n"
-			        	//}
-			        	depts_cont_data[i]=k+1
+			        	interComm_cont_data[i]=k+1
 			        	break
 		        	}
 	        	}*/
-	        	//0,0 : 124 309 311 313
-	        	/*if (px == image_width-1 && py == image_height-1){
-	        		console.log(lat,lng)
-	        		for (var k=0; k<voronoi_shape.features.length; ++k){
-						if (d3.geoContains(voronoi_shape.features[k],[lng,lat])){
-			        		console.log("matching with",k)
-			        		console.log(voronoi_shape.features[k])
-			        		console.log(voronoi_shape.features[k+1])
-			        	}
-		        	}
-	        	}*/
+
+	        	//
+	        	// ============ [2], voronoi below
+	        	//
+
         		/*for (var k=0; k<voronoi_shape.features.length; ++k){
 					if (d3.geoContains(voronoi_shape.features[k],[lng,lat])){
-		        		//voronoi_count[k]=voronoi_count[k]+1
-		        		//voronoi_means[k]=voronoi_means[k]+value
-		        		depts_cont_data[i]=k+1
+		        		voronoi_cont_data[i]=k+1
 		        		break
 		        	}
 	        	}*/
+
+	        	//
+	        	// ============ [3]
+	        	//
 	        	
 	        	
 	        	if (px == 0){
-	        		console.log(py)
+	        		console.log(py+"/"+height)
 	        	}
-	        	//console.log(i)
-	        }
-	        else{
-	        	pixels_data[i]=0;
 	        }
 	    }
-	    console.log(depts_cont_data)
-	    //console.log(voronoi_count[123])
-	    //console.log(voronoi_count[124])
-	    //console.log(voronoi_count[125])
-		var voronoi_string = ""
-		for (var i=0; i<voronoi_shape.features.length;++i){
-			if (voronoi_count[i] == 0){
-				voronoi_means[i]=0
-			}
-			else{
-				voronoi_means[i]=voronoi_means[i]/voronoi_count[i]
-			}
-			voronoi_string = voronoi_string+voronoi_means[i]+"\n"
+
+	    function download(text, name) {
+			d3.select(".container").append("a").attr("id","a"+name)
+			var a = document.getElementById("a"+name);
+			var file = new Blob([text], {type: 'json'});
+			a.href = URL.createObjectURL(file);
+			a.innerHTML="Click here to download "+name
+			d3.select(".container").append("br")
+			a.download = name;
 		}
-	    //console.log(JSON.stringify(pixels_data))
-	    function download(text, name, type, id) {
-	      d3.select(".container").append("a").attr("id","a"+id)
-		  var a = document.getElementById("a"+id);
-		  var file = new Blob([text], {type: type});
-		  a.href = URL.createObjectURL(file);
-		  a.innerHTML="Click here to download points_"+id+".txt"
-	      d3.select(".container").append("br")
-		  a.download = name;
-		}
-		console.log(img.width,img.height)
-		/*download(JSON.stringify({"width":img.width,
-								"height":img.height,
-								"tl_lat":tl.lat,
-								"tl_lng":tl.lng,
-								"br_lat":br.lat,
-								"br_lng":br.lng,
-								"data":JSON.stringify(pixels_data)}),"p_export.json","json",12)*/
-		download(JSON.stringify({"width" : img.width,
-								"height" : img.height,
-								"data" : JSON.stringify(depts_cont_data)}),"voronoi_cont.json","json",12)
-		for (var i=0; i<8; ++i){
 
-			download(string[i],"points_"+i+".txt","txt",i)
-		}
-		download(voronoi_string,"voronoi_means_p.txt","txt",9)
-	    // we put this random image in the context
-	    context.putImageData(imageData, 0, 0); // at coords 0,0
+		download(JSON.stringify({"width" : width,
+								"height" : height,
+								"data" : JSON.stringify(interComm_cont_data)}),"interComm_cont.json")
+		download(JSON.stringify({"width" : width,
+								"height" : height,
+								"data" : JSON.stringify(voronoi_cont_data)}),"voronoi_cont.json")
 
 
-	    var value=canvas.toDataURL("png");
-	    document.getElementById("my-second-image").src=value
-
-	    imgs.attr("xlink:href",value)
-	    console.log("ok!")
 	}
 });
 
