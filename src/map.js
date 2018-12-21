1903d33f474f8e0bf69242f12c3479be966dcb46
Alex Ferragni <pokemaniak@sunrise.ch>
2018-12-21 19:00:16 +0100

    clean map.js

--- src/map.js
+++ src/map.js
@@ -7,6 +7,7 @@ import shapes_f from './shapes.js'
 import shared from './shared.js'
 
 export default function(element, EV_svg_element, EV_circle_svg_element, legend_element, error, interComm_shape, voronoi_shape, interCommContainment, voronoiContainment, onHistChange, onSchools, onNewName) {
+  //for convenience
   shared.interComm_shape = interComm_shape
   shared.voronoi_shape = voronoi_shape
   shared.onHistChange = onHistChange
@@ -17,22 +18,27 @@ export default function(element, EV_svg_element, EV_circle_svg_element, legend_e
   shared.voronoiContainment = voronoiContainment
   shared.mapElement = element
 
-  document.body.addEventListener("keypress",helpers_f.handleKeyEvent)
+  document.body.addEventListener("keypress",helpers_f.handleKeyEvent) //add a keyboard listener to the main container 
 
+  //default top left position
   const default_tl = new L.LatLng(
     Config.viewport.topLatitude,
     Config.viewport.leftLongitude
   )
+  //default bottom right position
   const default_br = new L.LatLng(
     Config.viewport.bottomLatitude,
     Config.viewport.rightLongitude
   )
+  //set view to initial bounds
   shared.initialBounds = L.latLngBounds(default_tl, default_br)
 
+  //for now, hide the legend
   d3.select(legend_element).style("display","none")
+
+  //create legend canvas and svg
   shared.color_legend_canvas = d3.select(legend_element)
     .append("canvas")
-
   shared.color_legend_svg = d3.select(legend_element)
     .append("svg")
   shared.color_legend_svg.append("g")
@@ -52,14 +58,16 @@ export default function(element, EV_svg_element, EV_circle_svg_element, legend_e
 
   setLocationFromCurrent()
 
-  helpers_f.createMap(element) //TODO: if the window is small, the zoom is too far (> 14) and the map is grey, untill we zoom in
+  helpers_f.createMap(element)//create the map
 
+  //show a default hidden layer on the map to force it to create its overlay svg, for practical purposes (this one follows transitions smoothly!)
   L.geoJson(interComm_shape, {
     style: helpers_f.blankStyle
-  }).addTo(shared.map); //needed! otherwise a svg isn't generated, we use this one for practical purposes
+  }).addTo(shared.map);
 
   var svg = d3.select(element).select(".leaflet-zoom-animated")
 
+  //from lat lng to x,y inside the svg
   function projectPoint(x, y) {
     var point = shared.map.latLngToLayerPoint(new L.LatLng(y, x));
     this.stream.point(point.x, point.y);
@@ -70,6 +78,7 @@ export default function(element, EV_svg_element, EV_circle_svg_element, legend_e
   shared.pathGenerator = d3.geo.path()
     .projection(transform);
 
+  //used later to clip the layer image to a selected voronoi
   var defs = svg.append("defs");
   shared.defs_path = defs.append("path")
     .attr("id", "clip_path")
@@ -79,6 +88,7 @@ export default function(element, EV_svg_element, EV_circle_svg_element, legend_e
     .append("use")
     .attr("xlink:href", "#clip_path");
 
+  //define the layer image, the one that contains the real data (not average!)
   shared.imgs = svg.selectAll("image").data([0])
     .enter()
     .append("svg:image")
@@ -89,18 +99,22 @@ export default function(element, EV_svg_element, EV_circle_svg_element, legend_e
     .attr("preserveAspectRatio","none")
     .style("object-fit","cover")
 
+  //initialize the clip (it will hide the image entirely for now)
   update_f.update_clip()
 
 
   var fadedOpacity = 0.4
   var semiFullOpacity = 0.7
 
+  //svg used by the circle preview
   shared.svg_EV = d3.select(EV_svg_element)
     .attr("style","display:none;")
 
+  //svg used by the 
   shared.svg_circle_EV = d3.select(EV_circle_svg_element)
     .attr("style","display:none;")
 
+  //svg used to display the current layer image on the map
   shared.imgs_EV = shared.svg_EV.selectAll("image").data([0])
     .enter()
     .append("svg:image")
@@ -110,27 +124,20 @@ export default function(element, EV_svg_element, EV_circle_svg_element, legend_e
     .attr("preserveAspectRatio","none")
     .style("object-fit","cover")
 
-  var shape_EV = shared.svg_EV.append("g")
-  shape_EV
-    .selectAll("path")
-    .data([interComm_shape.features[0]])
-    .enter().append('path')
-    .attr('vector-effect', 'non-scaling-stroke')
-    .style('stroke', "#000")
-    .style("fill-opacity", 0)
-    .style("stroke-opacity", 1)
-
-
+  //define the voronoi shapes
   shared.voronoi = shapes_f.defineVoronoi(svg)
 
+  //define the intercomm shapes
   shared.interComms = shapes_f.defineInterComms(svg,fadedOpacity)
 
+  //marker used to pin location
   shared.markerIcon = {
     iconUrl: 'assets/marker.png',
     iconSize: [24, 24],
     iconAnchor: [12, 24]
   }
 
+  //create the marker element
   shared.markerElement = svg.append("g").selectAll("image").data([0])
     .enter()
     .append("svg:image")
@@ -140,12 +147,15 @@ export default function(element, EV_svg_element, EV_circle_svg_element, legend_e
     .attr('visibility', 'hidden')
     .style("pointer-events", "none")
 
+  //create a canvas, used to compute layer images
   shared.canvas = document.createElement("canvas")
 
+  //a dict of parameters used by the update functions. Will be filled later when needed
   shared.update_parameters = {
     hide: true,
   };
 
+  //add a listener on the map, will be called whenever we need to re-draw the overlay
   shared.map.on('viewreset', function(){update_f.updateMap()})
 
   const zoomOut = shared.map.zoomOut.bind(shared.map)
